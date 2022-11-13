5cebb15b2ce944cde8fd90b2372a52e0d14b6d02
AMundewal <abhishek.mundewal@forcam.com>
2022-11-13 12:00:12 +0100

    Removed not required code files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import SearchIcon from "./search.svg";
+import MovieCard from './MovieCard';
 
+
+const API_URL = "http://www.omdbapi.com/?apikey=26dc6eb6"
 function App() {
+
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  }
+
+  useEffect(() => {
+    searchMovies('Batman');
+  }, []);
+
+  /* 
+    const movie1 = {
+      "Title": "Batman Begins",
+      "Year": "2005",
+      "imdbID": "tt0372784",
+      "Type": "movie",
+      //"Poster": "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+      "Poster" : "N/A"
+    } */
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className='search'>
+        
+        <input placeholder='Search for movies' value={searchTerm} onChange={(event) => {setSearchTerm(event.target.value) }} onKeyPress={(event) => {
+          if (event.key === 'Enter'){
+             searchMovies(searchTerm) 
+          }
+        }
+        }/>
+
+        <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)}/>
+      
+      </div>
+
+      {movies?.length > 0
+        ? (
+            <div className='container'>
+              {movies.map((mov) => (
+                <MovieCard movie={mov} />
+              ))}
+            </div>
+          ) : (
+            <div className='empty'>
+              <h2>No Movies found</h2>
+            </div>
+          )}
+
     </div>
   );
 }
